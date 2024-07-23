8a2176f31be80d14c768318c3313072e1199f4f2
Prazpa <prapkornpm2001@gmail.com>
2024-07-23 13:09:24 +0700

    Create Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,67 @@
 "use client";
 
 import NavbarComponent from "@/components/Navbar/NavbarComponent";
+import { Button, Input, Select, SelectItem } from "@nextui-org/react";
+import { Noto_Serif } from "next/font/google";
+
+const noto = Noto_Serif({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
-      <NavbarComponent/>
+      <NavbarComponent />
+
+      {/* cover */}
+      <div className="w-fulll flex h-[900px] flex-col items-center justify-center bg-[url('/cover.jpg')]">
+        <span
+          className={`${noto.className} h-[220px] w-auto px-[80px] text-center text-[88px] font-semibold text-white`}
+        >
+          A Best Place for Your Neatly Experience
+        </span>
+
+        <div className="my-[50px] flex h-[196px] w-[1120px] items-center justify-around rounded-md bg-white opacity-90">
+          <div>
+            <span>Check In</span>
+            <Input
+              type="date"
+              className="h-[48px] w-[240px] rounded-lg border border-gray-500 text-gray-400"
+            />
+          </div>
+
+          <div>
+            <span>Check Out</span>
+            <Input
+              type="date"
+              className="h-[48px] w-[240px] rounded-lg border border-gray-500 text-gray-400"
+            />
+          </div>
+
+          <div>
+            <span>Room and Guests</span>
+            <Select className="h-[48px] w-[240px] rounded-lg border border-gray-500 text-gray-400">
+              <SelectItem key="1" className="bg-white">1 room 1,guests</SelectItem>
+              <SelectItem key="2" className="bg-white">1 room 2,guests</SelectItem>
+            </Select>
+          </div>
+
+          <Button type="submit" className="rounded-lg bg-orange-400 text-white ">
+            Search
+          </Button>
+        </div>
+
+        {/* Footer */}
+        <div className="w-full h-[485px] bg-[#2F3E35] text-white flex justify-between px-[80px]">
+          <div>
+            <h1>Neatly</h1>
+            <span>Neatly Hotel</span>
+            <span>The best hotel for rising your experience</span>
+          </div>
+
+          <div>
+            <h1>Contact</h1>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/Navbar/NavbarComponent.tsx
+++ src/components/Navbar/NavbarComponent.tsx
@@ -5,37 +5,90 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
-} from "@nextui-org/navbar";
-import { Button } from "@nextui-org/react";
+  Button,
+} from "@nextui-org/react";
+import { Noto_Serif } from "next/font/google";
+
+const noto = Noto_Serif({ subsets: ["latin"] });
 
 const NavbarComponent = () => {
   return (
     <Navbar className="h-[100px]">
-      <NavbarBrand>
-        <p className="text-3xl font-bold text-[#2F3E35] text-inherit">NEATLY</p>
+      <NavbarBrand className={noto.className}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={40}
+          height={40}
+          viewBox="0 0 512 512"
+        >
+          <defs>
+            <linearGradient
+              id="meteoconsStarFill0"
+              x1={187.9}
+              x2={324.1}
+              y1={138.1}
+              y2={373.9}
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop offset={0} stopColor="#E76B39"></stop>
+              <stop offset={0.5} stopColor="#E76B39"></stop>
+              <stop offset={1} stopColor="#E76B39fccd34"></stop>
+            </linearGradient>
+          </defs>
+          <path
+            fill="url(#meteoconsStarFill0)"
+            stroke="#E76B39"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={4}
+            d="m105.7 263.5l107.5 29.9a7.9 7.9 0 0 1 5.4 5.4l29.9 107.5a7.8 7.8 0 0 0 15 0l29.9-107.5a7.9 7.9 0 0 1 5.4-5.4l107.5-29.9a7.8 7.8 0 0 0 0-15l-107.5-29.9a7.9 7.9 0 0 1-5.4-5.4l-29.9-107.5a7.8 7.8 0 0 0-15 0l-29.9 107.5a7.9 7.9 0 0 1-5.4 5.4l-107.5 29.9a7.8 7.8 0 0 0 0 15Z"
+          >
+            <animateTransform
+              additive="sum"
+              attributeName="transform"
+              calcMode="spline"
+              dur="6s"
+              keySplines=".42, 0, .58, 1; .42, 0, .58, 1"
+              repeatCount="indefinite"
+              type="rotate"
+              values="-15 256 256; 15 256 256; -15 256 256"
+            ></animateTransform>
+            <animate
+              attributeName="opacity"
+              dur="6s"
+              values="1; .75; 1; .75; 1; .75; 1"
+            ></animate>
+          </path>
+        </svg>
+        <p className={`text-3xl font-bold text-[#2F3E35] text-inherit`}>
+          NEATLY
+        </p>
       </NavbarBrand>
-      <NavbarContent className="hidden gap-4 sm:flex" justify="center">
+      <NavbarContent className="hidden gap-5 sm:flex" justify="center">
         <NavbarItem>
-          <Link color="foreground" href="#">
+          <Link color="foreground" href="#" className="text-sm">
             About Neatly
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#">
+          <Link color="foreground" href="#" className="text-sm">
             Service & Facilities
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#">
-            Room & Suits
+          <Link color="foreground" href="#" className="text-sm">
+            Room & Suites
           </Link>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
-        <Button as={Link} color="primary" href="/login" variant="flat" className="text-orange-600">
+        <Button
+          as={Link}
+          color="primary"
+          href="/login"
+          variant="flat"
+          className="text-orange-600 text-sm"
+        >
           Login
         </Button>
       </NavbarContent>
